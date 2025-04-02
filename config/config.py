08e536ad89fd5488d8b85d7e9d2c78700064da96
DiasDavi <davi.dias271@gmail.com>
2025-04-01 22:53:43 -0300

    fix: arruma as constantes

--- config/config.py
+++ config/config.py
@@ -1,11 +1,7 @@
 import os
 
 BASE_PATH = "dataset"
-IMAGES_PATH = [
-    os.path.sep.join([BASE_PATH, "dayClip1"]),
-    os.path.sep.join([BASE_PATH, "dayClip2"]),
-    os.path.sep.join([BASE_PATH, "dayClip3"])
-]
+IMAGES_PATH = os.path.sep.join([BASE_PATH, "images"])
 ANNOTS_PATH = os.path.sep.join([BASE_PATH, "annotations.json"])
 
 BASE_OUTPUT = "output"
@@ -14,6 +10,8 @@ MODEL_PATH = os.path.sep.join([BASE_OUTPUT, "detector.h5"])
 PLOT_PATH = os.path.sep.join([BASE_OUTPUT, "plot.png"])
 TEST_FILENAMES = os.path.sep.join([BASE_OUTPUT, "test_files.txt"])
 
+
 INIT_LR = 1e-4
-NUM_EPOCHS = 5
-BATCH_SIZE = 32
+NUM_EPOCHS = 25
+BATCH_SIZE = 4
+NUM_MAX_BBOX = 2
